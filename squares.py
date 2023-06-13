998d8d9934915dd5e1a0d33dded81189aa3600c9
M Vamsi kiran <77319828+vamsinittphd@users.noreply.github.com>
2023-06-13 12:39:12 +0530

    Create squares.py

---
+++ squares.py
@@ -0,0 +1,8 @@
+# Task
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers i<n, print i^2.
+if __name__ == '__main__':
+    n = int(input())
+    i=0
+    for i in range (i,n):
+        print(i*i)
+    
