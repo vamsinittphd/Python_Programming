27d839be96adfccf76fbeebc2856b9c76dbb0bc4
M Vamsi kiran <77319828+vamsinittphd@users.noreply.github.com>
2023-06-13 12:02:08 +0530

    Create 03.arithmetic.py

---
+++ 03.arithmetic.py
@@ -0,0 +1,13 @@
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print("The sum of two numbers is:",a+b)
+    print("The difference of two numbers is:",a-b)
+    print("The product of two numbers is:",a*b)
